688c90682d46aa4496549cba9ddbd4f5b95cf436
nikitaojamae <nikita205>
2023-03-24 19:47:39 +0200

    4. Session material

--- src/main/java/ee/mainor/classroom/repository/GuessingGameRepository.java
+++ src/main/java/ee/mainor/classroom/repository/GuessingGameRepository.java
@@ -3,10 +3,10 @@ package ee.mainor.classroom.repository;
 import ee.mainor.classroom.model.GuessingGame;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface GuessingGameRepository extends JpaRepository<GuessingGame, Long> {
-
-
+    List<GuessingGame> findAllByCorrectNumber(Integer correctNumber);
 
 }
